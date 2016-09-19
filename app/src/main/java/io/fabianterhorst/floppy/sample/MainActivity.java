d1dc3ace12d81fd4ede1790c280f07fae307f73d
fabianterhorst <fabian.terhorst@gmail.com>
2016-09-19 16:48:15 +0200

    add included dependencies jar
    increase version

--- app/src/main/java/io/fabianterhorst/floppy/sample/MainActivity.java
+++ app/src/main/java/io/fabianterhorst/floppy/sample/MainActivity.java
@@ -4,8 +4,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import io.fabianterhorst.floppy.Disk;
 import io.fabianterhorst.floppy.Floppy;
+import io.fabianterhorst.floppy.OnWriteListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,10 +21,42 @@ public class MainActivity extends AppCompatActivity {
 
         Floppy.init(getFilesDir().toString());
 
-        Disk disk = Floppy.disk();
+        Log.d("path", getFilesDir().toString());
 
+        for(File file : getFilesDir().listFiles()) {
+            Log.d("path", file.getName());
+        }
+
+        Disk disk = Floppy.memoryDisk();
+        List<Integer> integers = new ArrayList<>();
+        for (int i = 0;i < 500; i++) {
+            integers.add(500);
+        }
+        long ts = System.currentTimeMillis();
+        disk.write("bla123", integers);
+        Log.d("time write", String.valueOf(System.currentTimeMillis() - ts));
         disk.write("bla", "bla2");
-
+        disk.write("bla", "bla3");
+        ts = System.currentTimeMillis();
         Log.d("value", disk.<String>read("bla"));
+        Log.d("time read", String.valueOf(System.currentTimeMillis() - ts));
+
+        disk.setOnWriteListener("bla", new OnWriteListener<String>() {
+            @Override
+            public void onWrite(String value) {
+                Log.d("valueChange", value);
+            }
+        });
+        disk.write("bla", "bla4");
+        disk.write("bla2", "bla5");
+        disk.write("bla", "bla6");
+
+        //for cache Floppy.memoryDisk()
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Floppy.disk().removeListener("bla");
     }
 }
