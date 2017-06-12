20d16995770d7a324477a93f447ba342276b9d0c
fabianterhorst <fabian.terhorst@gmail.com>
2017-06-12 16:03:38 +0200

    Use ints for disk type request and replace disk map with linked list

--- library/src/main/java/io/fabianterhorst/floppy/Floppy.java
+++ library/src/main/java/io/fabianterhorst/floppy/Floppy.java
@@ -2,19 +2,24 @@ package io.fabianterhorst.floppy;
 
 import org.nustaq.serialization.FSTConfiguration;
 
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Floppy {
 
     private static final String DEFAULT_DB_NAME = "default.disk";
 
-    private static final String DB_TYPE_MEMORY = "memory";
+    private static final int DB_TYPE_DEFAULT = 0;
 
-    private static final String DB_TYPE_ARRAY = "array";
+    private static final int DB_TYPE_MEMORY = 1;
+
+    private static final int DB_TYPE_ARRAY = 2;
 
     private static String mPath;
 
-    private static final ConcurrentHashMap<String, Disk> mDiskMap = new ConcurrentHashMap<>();
+    //private static final ConcurrentHashMap<String, Disk> mDiskMap = new ConcurrentHashMap<>();
+
+    private static final Map<String, Disk> mDiskMap = new LinkedHashMap<>();
 
     private static final FSTConfiguration mConfig = FSTConfiguration.createDefaultConfiguration();
 
@@ -26,7 +31,6 @@ public class Floppy {
         mPath = path;
         mConfig.setStructMode(true);
         mConfig.getCLInfoRegistry().setIgnoreAnnotations(true);
-        mConfig.setPreferSpeed(true);
     }
 
     public static void init(String path, Class... classes) {
@@ -38,26 +42,20 @@ public class Floppy {
         mConfig.registerClass(classes);
     }
 
-    private static <T> T disk(String name, String type) {
-        if (name.equals(DEFAULT_DB_NAME)) throw new RuntimeException(DEFAULT_DB_NAME +
-                " name is reserved for default library name");
-        return getDisk(name, type);
-    }
-
     public static Disk disk(String name) {
-        return disk(name, null);
+        return getDisk(name, DB_TYPE_DEFAULT);
     }
 
     public static Disk memoryDisk(String name) {
-        return disk(name, DB_TYPE_MEMORY);
+        return getDisk(name, DB_TYPE_MEMORY);
     }
 
     public static ArrayDisk arrayDisk(String name) {
-        return disk(name, DB_TYPE_ARRAY);
+        return getDisk(name, DB_TYPE_ARRAY);
     }
 
     public static Disk disk() {
-        return getDisk(DEFAULT_DB_NAME, null);
+        return getDisk(DEFAULT_DB_NAME, DB_TYPE_DEFAULT);
     }
 
     public static Disk memoryDisk() {
@@ -69,16 +67,16 @@ public class Floppy {
     }
 
     @SuppressWarnings("unchecked")
-    private static <T> T getDisk(String name, String type) {
+    private static <T> T getDisk(String name, int type) {
         if (mPath == null) {
             throw new RuntimeException("Floppy.init is not called");
         }
         synchronized (mDiskMap) {
             Disk disk = mDiskMap.get(name);
             if (disk == null) {
-                if (type == null) {
+                if (type == DB_TYPE_DEFAULT) {
                     disk = new Disk(name, mPath, mConfig);
-                } else if (type.equals(DB_TYPE_MEMORY)){
+                } else if (type == DB_TYPE_MEMORY) {
                     disk = new MemoryDisk(name, mPath, mConfig);
                 } else {
                     disk = new ArrayDisk(name, mPath, mConfig);
