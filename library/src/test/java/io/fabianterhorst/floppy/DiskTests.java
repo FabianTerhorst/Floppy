f3c95f9048cb5399abf4fba48529d87d81e8cb53
fabianterhorst <fabian.terhorst@gmail.com>
2017-06-12 23:39:45 +0200

    Update tests

--- library/src/test/java/io/fabianterhorst/floppy/DiskTests.java
+++ library/src/test/java/io/fabianterhorst/floppy/DiskTests.java
@@ -24,13 +24,24 @@ public class DiskTests {
     public void init() throws IOException {
         File currentFolder = folder.getRoot();
         Floppy.init(currentFolder.getPath());
-        folder.newFolder("default.disk");
-        folder.newFile("default.disk/test.pt");
         disk = Floppy.disk();
     }
 
+    /*@Test
+    public void testDisk() throws IOException {
+        folder.newFolder("1.disk");
+        folder.newFolder("2.disk");
+        Assert.assertFalse(Floppy.disk("1.disk") instanceof MemoryDisk);
+        Assert.assertFalse(Floppy.disk("1.disk") instanceof ArrayDisk);
+        Assert.assertTrue(Floppy.memoryDisk("2.disk") instanceof MemoryDisk);
+        Assert.assertFalse(Floppy.memoryDisk("2.disk") instanceof ArrayDisk);
+    }*/
+
     @Test
-    public void testReadWriteValue() {
+    public void testReadWriteValue() throws IOException {
+        File defaultFolder = new File(folder.getRoot(), "default.disk");
+        defaultFolder.mkdir();
+        new File(defaultFolder, "test.pt");
         String value = "Hello";
         Assert.assertEquals(null, disk.read("test"));
         disk.write("test", value);
